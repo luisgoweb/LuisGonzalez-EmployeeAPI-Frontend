2bfd9dd77f087b450cd94c5cb4af2ed1cc777e64
luisgoweb <luismga2012@gmail.com>
2025-08-22 21:05:34 -0400

    feat: Add navigation links to Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,66 @@
 
-const Sidebar = () => {
-  return (
-    <div>
-        <h1>Sidebar</h1>
-    </div>
-  )
-}
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PeopleIcon from "@mui/icons-material/People";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import {
+  Divider,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
+const primaryColor = "#1D9A94";
+const secondaryColor = "#0D314C";
 
-export default Sidebar
+
+const Sidebar = () => {
+ return (
+   <Grid
+     sx={{
+       maxWidth: 240,
+       height: "100%",
+       bgcolor: secondaryColor,
+       color: "#fff",
+       p: 2,
+     }}
+   >
+     <List>
+       <ListItem disablePadding sx={{ mb: 2 }}>
+         <ListItemButton component={NavLink} to="/employees">
+           <ListItemIcon>
+             <DashboardIcon sx={{ color: primaryColor }} />
+           </ListItemIcon>
+           <ListItemText primary="Dashboard" />
+         </ListItemButton>
+       </ListItem>
+      
+       <Divider sx={{ bgcolor: primaryColor, my: 2 }} />
+
+
+       {/* Links de Navegación */}
+       <ListItem disablePadding>
+         <ListItemButton component={NavLink} to="/employees">
+           <ListItemIcon>
+             <PeopleIcon sx={{ color: "#fff" }} />
+           </ListItemIcon>
+           <ListItemText primary="Empleados" />
+         </ListItemButton>
+       </ListItem>
+       <ListItem disablePadding>
+         <ListItemButton component={NavLink} to="/add-employee">
+           <ListItemIcon>
+             <PersonAddIcon sx={{ color: "#fff" }} />
+           </ListItemIcon>
+           <ListItemText primary="Añadir Empleados" />
+         </ListItemButton>
+       </ListItem>
+     </List>
+   </Grid>
+ );
+};
+
+
+export default Sidebar;
