a08f3c68d796e885a37c152456dd72eb5165a8f7
luisgoweb <luismga2012@gmail.com>
2025-08-22 20:59:56 -0400

    feat: Add Dashboard and AddEmployee views

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+
+export const theme = createTheme ({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#0c304c',
+    },
+    secondary: {
+      main: '#1c9c94',
+    },
+  },
+});
